3dca964461aa622dfdbbf6e9b143cd4383cc2211
irmankurniawan11 <irmankurniawan99@gmail.com>
2023-07-12 18:09:45 +0700

    First Commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Source_Sans_3 } from 'next/font/google'
+import Head from 'next/head'
+
+const font = Source_Sans_3({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Sobat Bangun</title>
+      </Head>
+      <div className={font.className}>
+        <Component {...pageProps} />
+      </div>
+    </>
+  )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        'brand-red' : '#F5333F',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
